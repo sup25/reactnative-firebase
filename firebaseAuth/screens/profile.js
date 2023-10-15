02eb8910a343bb7f9357cd8923b352c06fbb0782
sup25 <suparnaa51@gmail.com>
2023-10-15 19:25:05 +0545

    update profile

--- firebaseAuth/screens/profile.js
+++ firebaseAuth/screens/profile.js
@@ -2,7 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const profile = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  );
 };
 
 export default profile;
